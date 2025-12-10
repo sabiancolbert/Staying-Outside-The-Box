11bdf73cf0425831fd2430441fc2c730cca850a8
Sabian Colbert <31480512+sabiancolbert@users.noreply.github.com>
2025-12-10 12:08:08 -0800

    Update Starfield.js

--- Starfield.js
+++ Starfield.js
@@ -236,7 +236,7 @@ function moveStars() {
       if (DIST_SQ > 4 && DIST_SQ < MAX_INFLUENCE) {
         // Base strength shared by attraction (always on) and repulsion (burst)
         const BASE_PULL =
-          0.05 *
+          0.008 *
           CLEANED_USER_SPEED *
           ((MAX_INFLUENCE - DIST_SQ) / MAX_INFLUENCE);
 
