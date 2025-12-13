4974d75de2b28b81002dec431fc1128d810f89cf
Sabian Colbert <31480512+sabiancolbert@users.noreply.github.com>
2025-12-12 18:25:47 -0800

    Update Starfield.js

--- Starfield.js
+++ Starfield.js
@@ -316,8 +316,8 @@ if (NORM_USER_SPEED > 0.001 && USER_DISTANCE < MAX_INFLUENCE) {
     }
     
     // Apply final movement, while easing back to passive movement and adding passive drift
-    STAR.x += STAR.vx * (NORM_USER_SPEED + 1) + PULL_X;
-    STAR.y += STAR.vy * (NORM_USER_SPEED + 1) + PULL_Y;
+    STAR.x += STAR.vx * (NORM_USER_SPEED * 10 + 1) + PULL_X;
+    STAR.y += STAR.vy * (NORM_USER_SPEED * 10 + 1) + PULL_Y;
 
 
 
