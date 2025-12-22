757986983e9c643d1b71d712665e4cf9638be43e
Sabian Colbert <31480512+sabiancolbert@users.noreply.github.com>
2025-12-22 08:04:39 -0600

    Update Keyboard Starfield.js

--- Keyboard Starfield.js
+++ Keyboard Starfield.js
@@ -1,6 +1,10 @@
 let USER_INPUT = 0;
-const getForceX = (value) => (window.KEYBOARD_FORCE_X = value);
-const getForceY = (value) => (window.KEYBOARD_FORCE_Y = value);
+
+const setForceX = (value) => (window.KEYBOARD_FORCE_X = value);
+const getForceX = () => window.KEYBOARD_FORCE_X;
+
+const setForceY = (value) => (window.KEYBOARD_FORCE_Y = value);
+const getForceY = () => window.KEYBOARD_FORCE_Y;
 
 window.addEventListener("keydown", (event) => {
   // Ignore held-down repeats
